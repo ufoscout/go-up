84a9a6276dfd4dee3d6eafe7c178a87b8d79cfb1
ufoscout <ufoscout@gmail.com>
2018-02-18 12:36:56 +0100

    Add string utils

---
+++ util/stringUtils.go
@@ -0,0 +1,106 @@
+package util
+
+import "strings"
+
+/*
+HasTokens returns true if the input contains at least one token
+delimited by startDelimiter and endDelimiter
+ */
+func HasTokens(input string, startDelimiter string, endDelimiter string) bool {
+	start := strings.Index(input, startDelimiter)
+	end := strings.LastIndex(input, endDelimiter)
+	return start >= 0 && end >= start;
+}
+
+/*
+FirstToken returns the first token delimited by the startDelimiter and endDelimiter.
+ Example:
+ startDelimiter = "${"
+ endDelimiter = "}"
+
+ - input = "abdc" -> empty {@link Optional}
+ - input = "${abcd}" -> "abcd"
+ - input = "${${${abcd}}}" -> "abcd"
+ - input = "aaa${abcd}aaa${efgh}" -> "abcd"
+ */
+func FirstToken(input string, startDelimiter string, endDelimiter string) *string {
+	if HasTokens(input, startDelimiter, endDelimiter) {
+		for strings.Contains(input, startDelimiter) {
+			start := strings.Index(input, startDelimiter)
+
+			input = input[start+len(startDelimiter):]
+
+			for strings.Contains(input, endDelimiter) {
+				input = input[:strings.Index(input, endDelimiter)]
+			}
+		}
+		return &input
+	}
+	return nil
+}
+
+/**
+ * AllTokens returns all tokens delimited by the startDelimiter and endDelimiter.
+ * Example:
+ * startDelimiter = "${"
+ * endDelimiter = "}"
+ *
+ * - input = "abcd" -> {}
+ * - input = "${abcd}" -> {"abcd"}
+ * - input = "${${${abcd}}}" -> {"abcd"}
+ * - input = "aaa${abcd}aaa${efgh}" -> {"abcd","efgh"}
+ *
+ */
+func AllTokens(input string, startDelimiter string, endDelimiter string) []string {
+	tokens := []string{}
+
+	token := FirstToken(input, startDelimiter, endDelimiter);
+	for token != nil {
+		tokenValue := *token
+		tokens = append(tokens, tokenValue)
+		index := strings.Index(input, tokenValue) + len(tokenValue) + len(endDelimiter)
+		input = input[index:]
+		token = FirstToken(input, startDelimiter, endDelimiter);
+	}
+
+	return tokens;
+}
+
+/**
+ * AllTokensDistinct returne all tokens delimited by the startDelimiter and endDelimiter.
+ * If distinct is true, it removes duplicated tokens.
+ * Example:
+ * startDelimiter = "${"
+ * endDelimiter = "}"
+ * distinc = true
+ *
+ * - input = "abcd" -> {}
+ * - input = "${abcd}" -> {"abcd"}
+ * - input = "${${${abcd}${abcd}}}" -> {"abcd"}
+ * - input = "__${abcd}__${efgh}__${abcd}" -> {"abcd","efgh"}
+ *
+ * @param input
+ * @param startDelimiter
+ * @param endDelimiter
+ * @param distinct whether to strip duplicated tokens
+ * @return
+ */
+func AllTokensDistinct(input string, startDelimiter string, endDelimiter string, distinct bool) []string {
+	tokens := AllTokens(input, startDelimiter, endDelimiter);
+	if distinct {
+		return unique(tokens)
+	}
+	return tokens;
+}
+
+func unique(slice []string) []string {
+	keys := make(map[string]bool)
+	list := []string{}
+	for _, entry := range slice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
---
+++ util/stringUtils_test.go
@@ -0,0 +1,206 @@
+package util
+
+import (
+	"testing"
+    "github.com/stretchr/testify/assert"
+)
+
+var startDelimiter string = "${"
+var endDelimiter string = "}"
+
+func TestStringsWithTokens(t *testing.T) {
+
+	assert.True(t, HasTokens("hello ${hi}", startDelimiter, endDelimiter ))
+	assert.True(t, HasTokens("hello ${}", startDelimiter, endDelimiter ))
+	assert.True(t, HasTokens("${hello}", startDelimiter, endDelimiter ))
+	assert.True(t, HasTokens("${hello}}", startDelimiter, endDelimiter ))
+	assert.True(t, HasTokens("${${hello}", startDelimiter, endDelimiter ))
+	assert.True(t, HasTokens("${${hello}}", startDelimiter, endDelimiter ))
+	assert.True(t, HasTokens("start ${hello}", startDelimiter, endDelimiter ))
+	assert.True(t, HasTokens("${hello} end", startDelimiter, endDelimiter ))
+
+	assert.False(t, HasTokens("${hello", startDelimiter, endDelimiter ))
+	assert.False(t, HasTokens("$hello}", startDelimiter, endDelimiter ))
+	assert.False(t, HasTokens("{hello}", startDelimiter, endDelimiter ))
+	assert.False(t, HasTokens("}hello ${", startDelimiter, endDelimiter ))
+}
+
+func TestShouldReturnTheFirstToken(t *testing.T) {
+
+	assert.Equal(t, "hello", *FirstToken("${hello}", startDelimiter, endDelimiter ))
+	assert.Equal(t, "hello1", *FirstToken("${hello1}, ${hello2}", startDelimiter, endDelimiter ))
+	assert.Equal(t, "hello2", *FirstToken("hello1 ${hello2}", startDelimiter, endDelimiter ))
+
+	assert.Equal(t, "abcd", *FirstToken("${${${abcd}}}", startDelimiter, endDelimiter ))
+	assert.Equal(t, "abcd", *FirstToken("aaa${abcd}aaa${efgh}", startDelimiter, endDelimiter ))
+
+	assert.Nil(t, FirstToken("hello", startDelimiter, endDelimiter ))
+}
+
+func Test_ShouldReturnEmptyArray(t *testing.T) {
+	assert.Equal(t,0, len(AllTokens("", "{", "}")));
+}
+
+func Test_ShouldReturnEmpty2(t *testing.T) {
+input := "asfasdfasfdasfdasf_${_asdasd";
+assert.True(t, len(AllTokens(input, startDelimiter, endDelimiter)) == 0);
+}
+
+
+func Test_ShouldReturnEmpty3(t *testing.T) {
+input := "asfasdfasfdasfdasf_}_asdasd";
+assert.True(t, len(AllTokens(input, startDelimiter, endDelimiter)) == 0);
+}
+
+
+func Test_ShouldReturnSimpleToken(t *testing.T) {
+input := "${TOKEN}";
+tokens := AllTokens(input, startDelimiter, endDelimiter);
+
+assert.Equal(t, 1, len(tokens));
+assert.Equal(t, "TOKEN", tokens[0]);
+}
+
+
+func Test_ShouldReturnTokenAtTheEnd(t *testing.T) {
+input := "asfasdfasfdasfdasf_${TOKEN}";
+tokens := AllTokens(input, startDelimiter, endDelimiter);
+
+assert.Equal(t, 1, len(tokens));
+assert.Equal(t, "TOKEN", tokens[0]);
+}
+
+
+func Test_ShouldReturnTokenAtTheBeginning(t *testing.T) {
+input := "${TOKEN}asfasdfasfd_${_asfdasf";
+tokens := AllTokens(input, startDelimiter, endDelimiter);
+
+assert.Equal(t, 1, len(tokens));
+assert.Equal(t, "TOKEN", tokens[0]);
+}
+
+
+func Test_ShouldReturnTokenInTheMiddle(t *testing.T) {
+input := "asfasdfasf${TOKEN}dasfdasf";
+tokens := AllTokens(input, startDelimiter, endDelimiter);
+
+assert.Equal(t, 1, len(tokens));
+assert.Equal(t, "TOKEN", tokens[0]);
+}
+
+
+func Test_ShouldReturnTokenInTheMiddle2(t *testing.T) {
+input := "asfas}dfasf${TOKEN}dasfdasf";
+tokens := AllTokens(input, startDelimiter, endDelimiter);
+
+assert.Equal(t, 1, len(tokens));
+assert.Equal(t, "TOKEN", tokens[0]);
+}
+
+
+func Test_ShouldReturnTokenInTheMiddle3(t *testing.T) {
+input := "asfas${dfasf${TOKEN}dasfdasf";
+tokens := AllTokens(input, startDelimiter, endDelimiter);
+
+assert.Equal(t, 1, len(tokens));
+assert.Equal(t, "TOKEN", tokens[0]);
+}
+
+
+func Test_ShouldReturnTokenInTheMiddle4(t *testing.T) {
+input := "asfas${dfasf${TOKEN}dasfd${asf";
+tokens := AllTokens(input, startDelimiter, endDelimiter);
+
+assert.Equal(t, 1, len(tokens));
+assert.Equal(t, "TOKEN", tokens[0]);
+}
+
+
+func Test_ShouldReturnTokenInTheMiddle5(t *testing.T) {
+input := "asfas${dfasf${TOKEN}dasfd}asf";
+tokens := AllTokens(input, startDelimiter, endDelimiter);
+
+assert.Equal(t, 1, len(tokens));
+assert.Equal(t, "TOKEN", tokens[0]);
+}
+
+
+func Test_ShouldReturnInnerToken(t *testing.T) {
+input := "asfas${dfasf${TOKEN}dasf}dasf";
+tokens := AllTokens(input, startDelimiter, endDelimiter);
+
+assert.Equal(t, 1, len(tokens));
+assert.Equal(t, "TOKEN", tokens[0]);
+}
+
+
+func Test_ShouldReturnNestedToken(t *testing.T) {
+input := "${${${TOKEN}}}";
+tokens := AllTokens(input, startDelimiter, endDelimiter);
+
+assert.Equal(t, 1, len(tokens));
+assert.Equal(t, "TOKEN", tokens[0]);
+}
+
+
+func Test_ShouldReturnAllTokens1(t *testing.T) {
+input := "___${____${TOKEN_1}__}___${TOKEN_2}__";
+tokens := AllTokens(input, startDelimiter, endDelimiter);
+
+assert.Equal(t, 2, len(tokens));
+assert.Equal(t, "TOKEN_1", tokens[0]);
+assert.Equal(t, "TOKEN_2", tokens[1]);
+}
+
+
+func Test_ShouldReturnAllTokens2(t *testing.T) {
+input := "___${____${TOKEN_1__}___${TOKEN_2}__";
+tokens := AllTokens(input, startDelimiter, endDelimiter);
+
+assert.Equal(t, 2, len(tokens));
+assert.Equal(t, "TOKEN_1__", tokens[0]);
+assert.Equal(t, "TOKEN_2", tokens[1]);
+}
+
+
+func Test_ShouldReturnAllTokens3(t *testing.T) {
+input := "___${____${TOKEN_2}_}___${TOKEN_2}__";
+tokens := AllTokens(input, startDelimiter, endDelimiter);
+
+assert.Equal(t, 2, len(tokens));
+assert.Equal(t, "TOKEN_2", tokens[0]);
+assert.Equal(t, "TOKEN_2", tokens[1]);
+}
+
+
+func Test_ShouldReturnAllTokens4(t *testing.T) {
+input := "___${____${TOKEN_1}_${TOKEN_2}_}___${TOKEN_2}_${${TOKEN_3}}_";
+tokens := AllTokensDistinct(input, startDelimiter, endDelimiter, false);
+
+assert.Equal(t, 4, len(tokens));
+assert.Equal(t, "TOKEN_1", tokens[0]);
+assert.Equal(t, "TOKEN_2", tokens[1]);
+assert.Equal(t, "TOKEN_2", tokens[2]);
+assert.Equal(t, "TOKEN_3", tokens[3]);
+}
+
+
+func Test_ShouldReturnUniqueTokens1(t *testing.T) {
+input := "___${____${TOKEN_2}_}___${TOKEN_2}__";
+tokens := AllTokensDistinct(input, startDelimiter, endDelimiter, true);
+
+assert.Equal(t, 1, len(tokens));
+assert.Equal(t, "TOKEN_2", tokens[0]);
+}
+
+
+func Test_ShouldReturnUniqueTokens2(t *testing.T) {
+input := "___${____${TOKEN_1}_${TOKEN_2}_}___${TOKEN_2}_${${TOKEN_3}}_";
+tokens := AllTokensDistinct(input, startDelimiter, endDelimiter, true);
+
+assert.Equal(t, 3, len(tokens));
+assert.Equal(t, "TOKEN_1", tokens[0]);
+assert.Equal(t, "TOKEN_2", tokens[1]);
+assert.Equal(t, "TOKEN_3", tokens[2]);
+}
+
