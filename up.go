32d237484ad40819fc5d33ed6ece4ef1fb70bcf3
ufoscout <ufoscout@gmail.com>
2018-02-18 19:23:55 +0100

    Add minimial GoUp implementation

--- up.go
+++ up.go
@@ -1,86 +1,28 @@
 package go_up
 
-import (
-	"github.com/ufoscout/go-up/reader/decorator"
-	"github.com/ufoscout/go-up/reader"
-)
-
-const HIGHEST_PRIORITY int = 0
-const DEFAULT_PRIORITY int = 100
-
-const DEFAULT_START_DELIMITER string = "${"
-const DEFAULT_END_DELIMITER string = "}"
-
-const DEFAULT_LIST_SEPARATOR string = ",";
+import "github.com/ufoscout/go-up/reader"
 
 type GoUp interface {
-}
-
-func NewGoUp() GoUp {
-	return &goUpImpl{decorator.NewPriorityQueueDecoratorReader(),
-		DEFAULT_START_DELIMITER,
-		DEFAULT_END_DELIMITER,
-		false}
+	GetOrDefault(key string, defaultValue string) string
+	Get(key string) *string
 }
 
 type goUpImpl struct {
-	reader                         *decorator.PriorityQueueDecoratorReader
-	startDelimiter                 string
-	endDelimiter                   string
-	ignoreUnresolvablePlaceholders bool
+	properties map[string]reader.Property
 }
 
-/**
- * Add a new property Reader with the default priority.
- * If two or more Readers have the same priority, the last added has the highest priority among them.
- *
- */
-func (up *goUpImpl) AddReader(newReader reader.Reader) GoUp {
-	return up.AddReaderWithPriority(newReader, DEFAULT_PRIORITY)
+func (up *goUpImpl) GetOrDefault(key string, defaultValue string) string {
+	value, found := up.properties[key]
+	if !found {
+		return defaultValue
+	}
+	return value.Value
 }
 
-/**
- * Add a new property Reader.
- * If two or more Readers have the same priority, the last added has the highest priority among them.
- *
- */
-func (up *goUpImpl) AddReaderWithPriority(newReader reader.Reader, priority int) GoUp {
-	up.reader.Add(up.reader, priority)
-	return up
-}
-
-/**
- * Add a new properties file with the default priority.
- * If two or more Readers have the same priority, the last added has the highest priority among them.
- */
-func (up *goUpImpl) AddFile(filename string, ignoreNotFound bool) GoUp {
-	return up.AddFileWithPriority(filename, ignoreNotFound, DEFAULT_PRIORITY)
-}
-
-/**
- * Add a new properties file with the specified priority.
- * If two or more Readers have the same priority, the last added has the highest priority among them.
- */
-func (up *goUpImpl) AddFileWithPriority(filename string, ignoreNotFound bool, priority int) GoUp {
-	return up.AddReaderWithPriority(&reader.FileReader{filename, ignoreNotFound}, priority)
-}
-
-/**
- *
- * Set the start and end placeholder delimiters.
- * Default are {@value Default#START_DELIMITER} and {@value Default#END_DELIMITER}
- */
-func (up *goUpImpl) Delimiters(startDelimiter string, endDelimiter string) GoUp {
-	up.startDelimiter = startDelimiter
-	up.endDelimiter = endDelimiter
-	return up
-}
-
-/**
- * Whether to ignore not resolvable placeholders.
- * Default is false.
- */
-func (up *goUpImpl) IgnoreUnresolvablePlaceholders(ignoreUnresolvablePlaceholders bool) GoUp {
-	up.ignoreUnresolvablePlaceholders = ignoreUnresolvablePlaceholders
-	return up
+func (up *goUpImpl) Get(key string) *string {
+	value, found := up.properties[key]
+	if !found {
+		return nil
+	}
+	return &value.Value
 }
---
+++ upBuilder.go
@@ -0,0 +1,106 @@
+package go_up
+
+import (
+	"github.com/ufoscout/go-up/reader/decorator"
+	"github.com/ufoscout/go-up/reader"
+)
+
+const HIGHEST_PRIORITY int = 0
+const DEFAULT_PRIORITY int = 100
+
+const DEFAULT_START_DELIMITER string = "${"
+const DEFAULT_END_DELIMITER string = "}"
+
+const DEFAULT_LIST_SEPARATOR string = ",";
+
+type GoUpBuilder interface {
+	AddReader(newReader reader.Reader) GoUpBuilder
+	AddReaderWithPriority(newReader reader.Reader, priority int) GoUpBuilder
+	AddFile(filename string, ignoreNotFound bool) GoUpBuilder
+	AddFileWithPriority(filename string, ignoreNotFound bool, priority int) GoUpBuilder
+	Delimiters(startDelimiter string, endDelimiter string) GoUpBuilder
+	IgnoreUnresolvablePlaceholders(ignoreUnresolvablePlaceholders bool) GoUpBuilder
+	build() (GoUp, error)
+}
+
+func NewGoUp() GoUpBuilder {
+	return &goUpBuilderImpl{decorator.NewPriorityQueueDecoratorReader(),
+		DEFAULT_START_DELIMITER,
+		DEFAULT_END_DELIMITER,
+		false}
+}
+
+type goUpBuilderImpl struct {
+	reader                         *decorator.PriorityQueueDecoratorReader
+	startDelimiter                 string
+	endDelimiter                   string
+	ignoreUnresolvablePlaceholders bool
+}
+
+/**
+ * Add a new property Reader with the default priority.
+ * If two or more Readers have the same priority, the last added has the highest priority among them.
+ *
+ */
+func (up *goUpBuilderImpl) AddReader(newReader reader.Reader) GoUpBuilder {
+	return up.AddReaderWithPriority(newReader, DEFAULT_PRIORITY)
+}
+
+/**
+ * Add a new property Reader.
+ * If two or more Readers have the same priority, the last added has the highest priority among them.
+ *
+ */
+func (up *goUpBuilderImpl) AddReaderWithPriority(newReader reader.Reader, priority int) GoUpBuilder {
+	up.reader.Add(up.reader, priority)
+	return up
+}
+
+/**
+ * Add a new properties file with the default priority.
+ * If two or more Readers have the same priority, the last added has the highest priority among them.
+ */
+func (up *goUpBuilderImpl) AddFile(filename string, ignoreNotFound bool) GoUpBuilder {
+	return up.AddFileWithPriority(filename, ignoreNotFound, DEFAULT_PRIORITY)
+}
+
+/**
+ * Add a new properties file with the specified priority.
+ * If two or more Readers have the same priority, the last added has the highest priority among them.
+ */
+func (up *goUpBuilderImpl) AddFileWithPriority(filename string, ignoreNotFound bool, priority int) GoUpBuilder {
+	return up.AddReaderWithPriority(&reader.FileReader{filename, ignoreNotFound}, priority)
+}
+
+/**
+ *
+ * Set the start and end placeholder delimiters.
+ * Default are {@value Default#START_DELIMITER} and {@value Default#END_DELIMITER}
+ */
+func (up *goUpBuilderImpl) Delimiters(startDelimiter string, endDelimiter string) GoUpBuilder {
+	up.startDelimiter = startDelimiter
+	up.endDelimiter = endDelimiter
+	return up
+}
+
+/**
+ * Whether to ignore not resolvable placeholders.
+ * Default is false.
+ */
+func (up *goUpBuilderImpl) IgnoreUnresolvablePlaceholders(ignoreUnresolvablePlaceholders bool) GoUpBuilder {
+	up.ignoreUnresolvablePlaceholders = ignoreUnresolvablePlaceholders
+	return up
+}
+
+func (up *goUpBuilderImpl) build() (GoUp, error) {
+
+	replacer := decorator.PlaceholderReplacerDecoratorReader{up.reader, up.startDelimiter,up.endDelimiter, up.ignoreUnresolvablePlaceholders}
+
+	properties, err := replacer.Read()
+
+	if err!=nil {
+		return nil, err
+	}
+
+	return &goUpImpl{properties}, nil
+}
