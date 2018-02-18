1189a5494066dff1e2641b713ecf2d0cd2895fac
ufoscout <ufoscout@gmail.com>
2018-02-18 18:24:30 +0100

    Add priority queue decorator

---
+++ reader/decorator/priorityQueueDecoratorReader.go
@@ -0,0 +1,65 @@
+package decorator
+
+import (
+	"github.com/ufoscout/go-up/reader"
+	"sort"
+)
+
+/**
+ * A Reader that wraps a prioritized list of other Readers.
+ * Each Reader in the list has a priority that is used to resolve conflicts
+ * when a key is defined more than once.
+ * If two or more Readers have the same priority, the one added by last has the highest priority
+ *
+ * @author Francesco Cina
+ *
+ */
+type PriorityQueueDecoratorReader struct {
+	ReadersMap map[int][]reader.Reader
+}
+
+func (f *PriorityQueueDecoratorReader) Read() (map[string]reader.Property, error) {
+
+	output := map[string]reader.Property{}
+
+	// To store the keys in slice in sorted order
+	var keys []int
+	for k := range f.ReadersMap {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+
+	for i := len(keys)-1; i >= 0; i-- {
+		key := keys[i]
+		value := f.ReadersMap[key]
+		for _, readers := range value {
+
+			properties, err := readers.Read()
+
+			if err!=nil {
+				return nil, err
+			}
+
+			for k,v := range properties {
+				output[k] = v
+			}
+		}
+	}
+	return output, nil
+
+}
+
+func (f *PriorityQueueDecoratorReader) Add(newReader reader.Reader, priority int) *PriorityQueueDecoratorReader {
+	readers, found := f.ReadersMap[priority]
+	if !found {
+		readers = []reader.Reader{}
+		f.ReadersMap[priority] = readers
+	}
+	readers = append(readers, newReader)
+	f.ReadersMap[priority] = readers
+	return f
+}
+
+func NewPriorityQueueDecoratorReader() *PriorityQueueDecoratorReader {
+	return &PriorityQueueDecoratorReader{map[int][]reader.Reader{}}
+}
--- reader/programmaticReader.go
+++ reader/programmaticReader.go
@@ -8,12 +8,14 @@ func (f *ProgrammaticReader) Read() (map[string]Property, error) {
 	return f.properties, nil
 }
 
-func (f *ProgrammaticReader) Add(key string, value string) {
+func (f *ProgrammaticReader) Add(key string, value string) *ProgrammaticReader {
 	f.AddProperty(key, Property{value, true})
+	return f
 }
 
-func (f *ProgrammaticReader) AddProperty(key string, value Property) {
+func (f *ProgrammaticReader) AddProperty(key string, value Property) *ProgrammaticReader {
 	f.properties[key] = value
+	return f
 }
 
 func NewProgrammaticReader() *ProgrammaticReader {
